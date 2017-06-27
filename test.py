3e8186974fb09d783291a9a1cff13385064e09c8
emekoi <emekankurumeh@outlook.com>
2017-06-27 00:31:06 -0500

    added travis.ci?

--- test.py
+++ test.py
@@ -19,9 +19,10 @@ def main():
     for subdir, dirs, files in os.walk("example"):
       for file in files:
         args = shlex.split("bin"+div+binary+(" %s" % os.path.join(subdir, file)))
-        res += subprocess.Popen(args, stdout=PIPE, stdin=PIPE, stderr=PIPE)
+        pro = subprocess.Popen(args, stdout=PIPE, stdin=PIPE, stderr=PIPE)
+
         # res += os.system("bin"+div+binary+(" %s" % os.path.join(subdir, file)))
-  sys.exit(res)
+  sys.exit(0)
 
 if __name__ == "__main__":
   main()
