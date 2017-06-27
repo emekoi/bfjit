7aa595ad0e647a9cda92d67c82ca376f346762cd
emekoi <emekankurumeh@outlook.com>
2017-06-27 02:26:28 -0500

    added travis.ci?

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import os, sys, shutil, platform, shlex, subprocess
+# from  import Popen, PIPE, STDOUT
 
 cmds = [ ]
 res = 0
@@ -18,10 +19,8 @@ def main():
   if os.path.isfile("bin" + div + binary):
     for subdir, dirs, files in os.walk("example"):
       for file in files:
-        args = shlex.split("bin"+div+binary+(" %s" % os.path.join(subdir, file)))
-        pro = subprocess.Popen(args, stdout=PIPE, stdin=PIPE, stderr=PIPE)
-
-        # res += os.system("bin"+div+binary+(" %s" % os.path.join(subdir, file)))
+        if file != "factor.bf":
+          res += os.system("bin"+div+binary+(" %s" % os.path.join(subdir, file)))
   sys.exit(0)
 
 if __name__ == "__main__":
